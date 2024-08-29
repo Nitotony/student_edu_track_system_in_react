ba07b1bb44c5f5188a2fdf75dc5da712e910ad5b
Nito Tony C <nitotony47@gmail.com>
2024-08-29 19:49:02 +0530

    partial not complete

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,22 +3,26 @@ import React from "react";
 import { HiUsers } from "react-icons/hi";
 import { ImDrawer2 } from "react-icons/im";
 import { BiSolidOffer } from "react-icons/bi";
-
+import { BsFileEarmarkBarGraphFill } from "react-icons/bs";
 import MenuItem from "./menuitem";
+import { CgCollage } from "react-icons/cg";
+import img from "../assets/Boston College.png"
 
+import './Menu.css'
 
 const menus=[
     {id:1,name:"Student Management",url:"/",logo:<HiUsers/>},
     {id:2,name:"Financial Management",url:"/",logo:<ImDrawer2/>},
     {id:3,name:"Quality Control",url:"/",logo:<BiSolidOffer/>},
-    {id:4,name:"Report Delivery",url:"/"},
-    {id:5,name:"Attendance",url:"/"},
+    {id:4,name:"Report Delivery",url:"/",logo:<CgCollage/>},
+    {id:5,name:"Attendance",url:"/", logo:<BsFileEarmarkBarGraphFill/>
+    },
 ]
 const Menu=()=>{
     return(
         <div className="Menu_body">
             <div className="Menu_logo">
-                <img src="" alt="" />
+                <img src={img} alt="" />
             </div>
             <div className="Menu_links">
 
--- src/components/menuitem.js
+++ src/components/menuitem.js
@@ -1,15 +1,13 @@
 import React from "react";
-
+import "./menuitem.css"
 const MenuItem = ({ url, name, logo }) => {
     return (
-        <a href={url} className="MenuItem">
-            <div className="MenuItem_logo">
-                {logo}
-            </div>
-            <div className="MenuItem_name">
-                {name}
-            </div>
-        </a>
+      <a href={url} className="MenuItem">
+        <div className="MenuItem_logo">
+          <span>{logo}</span>
+        </div>
+        <div className="MenuItem_name">{name}</div>
+      </a>
     );
 };
 
